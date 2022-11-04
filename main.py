12d8d830605d8ce35a3ae1db33d00ec626586ba7
Adithya <adithyam866@gmail.com>
2022-11-04 17:55:33 +0530

    Commented

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from dronekit import *
 
 #Connect to Drone
 vehicle = connect('127.0.0.1:14551',baud=921600, wait_ready=True)
-print(vehicle.mode)
-print(vehicle.is_armable)
-print(vehicle.armed)
+print(vehicle.mode)#Returns the status of the drone
+print(vehicle.is_armable)#Checks if armable
+print(vehicle.armed)#Returns if armed
